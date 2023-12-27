baa4181c80cd491940662c7b93c0380c2c5aaba8
stark5200 <a.taha5200@gmail.com>
2023-12-27 23:36:54 +0100

    good so far

--- problem1.ts
+++ problem1.ts
@@ -1,3 +1,7 @@
 function mergeAlternately(word1: string, word2: string): string {
-return(word1+word2)
+(totallen: Number) => word1.length + word2.length;
+(tl: String) => totallen.string;
+return(word1+word2+String(totallen));
 };
+
+mergeAlternately("hi", "yummy");
