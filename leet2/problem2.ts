6e42169d4ec49b9ad2be436834f807bd1115eaa8
stark5200 <a.taha5200@gmail.com>
2023-12-31 12:14:44 +0100

    'e', 'l', 'o'

--- leet2/problem2.ts
+++ leet2/problem2.ts
@@ -4,6 +4,7 @@ function gcdStrings(str1: string, str2: string): String {
   let short = "";
   let long = "";
   let mlen = Math.min(str1.length,  str2.length);
+  let Mlen = Math.max(str1.length,  str2.length);
   if (str1.length == mlen) {
     short = str1;
     long = str2;
@@ -14,12 +15,19 @@ function gcdStrings(str1: string, str2: string): String {
   console.log("shorter string is: '" + short + "', " + "longer string is: '" + long + "'.");
 
   for (let i = 0; i < mlen; i++) {
-    for (let j = 0; j < mlen; j++) {
-      result = "ij";
+    for (let j = 0; j < Mlen; j++) {
+      if( (short.charAt(i) == long.charAt(j)) && !matches.includes(short.charAt(i)) ) {
+        matches.push(short.charAt(i));
+        if( (short.charAt(i+1) == long.charAt(j+1)) && !matches.includes(short.charAt(i).concat(short.charAt(i+1))) ) {
+          matches.push(short.charAt(i).concat(short.charAt(i+1)));
+        }
+      }
     }
   }
+  console.log("current matches: ");
+  console.log(matches);
   return(result); 
 }
 
-gcdStrings("a22graf", "helloo");
+gcdStrings("love me", "hello");
         
--- problem1.ts
+++
@@ -1,7 +0,0 @@
-function mergeAlternately(word1: string, word2: string): string {
-(totallen: Number) => word1.length + word2.length;
-(tl: String) => totallen.string;
-return(word1+word2+String(totallen));
-};
-
-mergeAlternately("hi", "yummy");
