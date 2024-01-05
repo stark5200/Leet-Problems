ba741fc44beda04a13fd99049e72b88a0a497660
stark5200 <a.taha5200@gmail.com>
2024-01-05 13:51:06 +0100

    vowel letters

--- leet5/problem5.ts
+++ leet5/problem5.ts
@@ -1,5 +1,8 @@
 // to run code: ts-node problem3.ts, to compile new js version: tsc problem3.ts, to run js version: node problem3.ts
 
 function reverseVowels(s: string): string {
+  let vowels: string[] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let found = [];
+  
     return ""
 };
