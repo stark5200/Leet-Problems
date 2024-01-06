4576f46d607f1f9d72b1704c3e3bb17431cc555c
stark5200 <a.taha5200@gmail.com>
2024-01-06 10:58:59 +0100

    solved 5

--- leet5/problem5.ts
+++ leet5/problem5.ts
@@ -2,13 +2,49 @@
 
 function reverseVowels(s: string): string {
   let vowels: string[] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let found: string[] = [];
   let letters: string[] = s.split("");
-  console.log(letters)
+  let len = s.length;
 
-  //for (let char: string in s)
-  let found = [];
+  for (let i = 0; i < len; i++) {
+     vowels.includes(letters[i]) ? (found.push(letters[i]), letters[i] = "😎") : null
+  }
 
-    return ""
+  for (let j = 0; j < len; j++) {
+    letters[j] == "😎" ? letters[j] = found.pop() as string : null
+  }
+  console.log(letters);
+  
+  return letters.join("");
 };
 
 reverseVowels("Hello");
+
+/* Leet code solution
+
+function reverseVowels(s: string): string {
+    let arr = s.split('')
+    
+    let left = 0
+    let right = arr.length - 1
+    let n;
+    const set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+
+    while(left <= right){
+        if(set.has(arr[left]) && set.has(arr[right])){
+            n = arr[left]
+            arr[left] = arr[right]
+            arr[right] = n
+            left++
+            right--
+        }else if(!set.has(arr[left])){
+            left++
+        }else{
+            right--
+        }
+    }
+
+    return arr.join('')
+};
+
+*/
