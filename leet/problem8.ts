57d6fa634dfb42b0353effe3d0c46403571a3776
stark5200 <a.taha5200@gmail.com>
2024-01-09 18:51:15 +0100

    chatgpt solution

--- leet/problem8.ts
+++ leet/problem8.ts
@@ -22,3 +22,22 @@ function increasingTriplet(nums: number[]): boolean {
 
     return exist;
 };
+
+/* Chat gpt solution
+function increasingTriplet(nums: number[]): boolean {
+    let firstMin = Number.MAX_VALUE;
+    let secondMin = Number.MAX_VALUE;
+
+    for (let num of nums) {
+        if (num <= firstMin) {
+            firstMin = num;
+        } else if (num <= secondMin) {
+            secondMin = num;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+*/
