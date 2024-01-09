3cce421d468a6f4e1b6d818c598225e46aaeda1d
stark5200 <a.taha5200@gmail.com>
2024-01-09 18:39:50 +0100

    halfway solution

--- leet/problem8.ts
+++ leet/problem8.ts
@@ -1,7 +1,24 @@
 function increasingTriplet(nums: number[]): boolean {
     let exist = false;
+    let len = nums.length;
+    let maxnum = Math.max(...nums); 
 
     //code goes here
+    for (let i = 0; i < len; i++) {
+        if (nums[i] == maxnum) {
+            continue;
+        }  
+        for (let j = i+1; j < len; j++) {
+            if (nums[j] == maxnum) {
+                continue;
+            }
+            for (let k = j+1; k < len; k++) {
+                if (nums[i] < nums[j] && nums[j] < nums[k]) {
+                    return true;
+                }
+            }
+        }   
+    }
 
     return exist;
 };
