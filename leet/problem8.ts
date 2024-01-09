f6b1a57fa4dae25304b3779e3f99625b7db951ae
stark5200 <a.taha5200@gmail.com>
2024-01-09 20:06:02 +0100

    compress

--- leet/problem8.ts
+++ leet/problem8.ts
@@ -1,4 +1,29 @@
 function increasingTriplet(nums: number[]): boolean {
+
+    let firstMin = Number.MAX_VALUE;
+    let secondMin = Number.MAX_VALUE;
+    console.log("initial first Min:" + firstMin);
+    console.log("initial second Min:" + secondMin);
+
+    for (let num of nums) {
+        if (num <= firstMin) {
+            firstMin = num;
+            console.log("first Min:" + firstMin);
+        } else if (num <= secondMin) {
+            secondMin = num;
+            console.log("second Min:" + secondMin);
+        } else {
+            return true;
+        }
+    }
+    
+    return false;
+};
+
+let testcase = [10, 5, 2, 7, -2, 11];
+
+console.log(increasingTriplet(testcase));
+    /* bad code
     let exist = false;
     let len = nums.length;
     let maxnum = Math.max(...nums); 
@@ -21,23 +46,9 @@ function increasingTriplet(nums: number[]): boolean {
     }
 
     return exist;
-};
+    */
 
-/* Chat gpt solution
-function increasingTriplet(nums: number[]): boolean {
-    let firstMin = Number.MAX_VALUE;
-    let secondMin = Number.MAX_VALUE;
 
-    for (let num of nums) {
-        if (num <= firstMin) {
-            firstMin = num;
-        } else if (num <= secondMin) {
-            secondMin = num;
-        } else {
-            return true;
-        }
-    }
+/* community solution
 
-    return false;
-}
 */
