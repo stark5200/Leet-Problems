170700164c967e44dd6bc2ee0df4a61f591ebf59
stark5200 <a.taha5200@gmail.com>
2024-01-11 04:45:52 +0100

    commit for now

--- leet/problem9.ts
+++ leet/problem9.ts
@@ -1,3 +1,17 @@
 function compress(chars: string[]): number {
-    return 20;
+    let init = chars[0]; 
+    let count = 0;
+    let compressed: string[] = [];
+    for (let char in chars) {
+        console.log(char);
+        if (char == init) {
+            count++;
+        } else { (compressed.push(init), compressed.push(count.toString()) , init = char , count = 1) };
+        console.log(count.toString() + init + count);
+    }
+    console.log(compressed)
+    return compressed.length;
 };
+
+let uncompressed = ["a","a","b","b","c","c","c"];
+console.log(compress(uncompressed));
