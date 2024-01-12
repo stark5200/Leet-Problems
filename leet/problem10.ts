c1f95360350be616cf671c5067c3ad9ec0333b6e
stark5200 <a.taha5200@gmail.com>
2024-01-12 19:15:39 +0100

    writer reader solution

--- leet/problem10.ts
+++ leet/problem10.ts
@@ -2,7 +2,24 @@
  Do not return anything, modify nums in-place instead.
  */
  function moveZeroes(nums: number[]): void {
-  for (let n = 0; n < nums.length; n++) {
-    nums[n] == 0 ? (nums.splice(n, 1), nums.push(0)) : null;
+  let i = 0;
+  for (let n = 0 ; n < nums.length; n++) {
+    nums[i] == 0 ? (nums.splice(i, 1), nums.push(0)) : i++;
   }  
 };
+
+/* Optimal solution
+
+function moveZeroes(nums: number[]): void {
+  let writePointer = 0;
+  for (let readPointer = 0; readPointer < nums.length; readPointer ++) {
+    const val = nums[readPointer];
+    nums[readPointer] = 0;
+    if (val!==0) {
+      nums[writePointer] = val;
+      writePointer ++;
+    }
+  }
+};
+
+*/
