11cbe2a4c0071c3722f4384950097a42985e7971
stark5200 <a.taha5200@gmail.com>
2024-01-12 19:08:43 +0100

    partial proof

--- leet/problem10.ts
+++ leet/problem10.ts
@@ -2,5 +2,7 @@
  Do not return anything, modify nums in-place instead.
  */
  function moveZeroes(nums: number[]): void {
-    
- };
+  for (let n = 0; n < nums.length; n++) {
+    nums[n] == 0 ? (nums.splice(n, 1), nums.push(0)) : null;
+  }  
+};
