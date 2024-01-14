9ff07863122fe63c8433ddfa6309493dec61ad04
stark5200 <a.taha5200@gmail.com>
2024-01-14 23:04:31 +0100

    start of 15

--- leet/problem14.ts
+++ leet/problem14.ts
@@ -1,16 +1,16 @@
 function findMaxAverage(nums: number[], k: number): number {
   let sum = (n: number, ...arr: number[]) => [...arr.slice(n, n+k)].reduce((x, y) => x + y, 0); 
   let max = sum(0, ...nums);
+  console.log("initial max is: "+max);
   let currentsum = max;
   for (let num = 1; num <= nums.length-k; num++) {
-    currentsum = currentsum-nums[num-1]+nums[num+k+1];
+    currentsum = currentsum-nums[num-1]+nums[num+k-1];
     max = Math.max(currentsum, max);
   }
-  console.log(max/k);
   return max/k;  
 };
 
-findMaxAverage([1, 3, 4, 5], 2);
+findMaxAverage([1, 3, 4, 5, 4, 1, 5, 3, 8, 9], 4);
 
 
 /*
