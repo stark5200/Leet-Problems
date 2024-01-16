b3a5d9d2d7fa3c5f11ec53376a33ae094b0dc9a3
stark5200 <a.taha5200@gmail.com>
2024-01-16 19:57:48 +0100

    finally done

--- leet/problem16.ts
+++ leet/problem16.ts
@@ -1,28 +1,37 @@
 function longestOnes(nums: number[], k: number): number {
   let maxOnes = 0;
+  let sortedNums = [...nums];
+  sortedNums = sortedNums.sort();
+  let zeros = sortedNums.indexOf(1);
+  if (k >= zeros && zeros > 0) { 
+    return nums.length;
+  }
+  if (k >= nums.length) {
+    return k;
+  }
   for (let num = 0; num <= nums.length - k; num++) {
     let ones = 0;
     let counter = num;
-    console.log("initial counter for num " + num + " is: " + counter + " , max ones is: " + maxOnes)
+    //console.log("initial counter for num " + num + " is: " + counter + " , max ones is: " + maxOnes)
     let extra = k;
     while (counter < nums.length && extra >= 0) {
       if (nums[counter]==1) {
         ones++;
         counter++;
-        console.log("current counter is: " + counter + " , current ones is: " + ones )
+        //console.log("current counter is: " + counter + " , current ones is: " + ones )
       } else if (nums[counter]==0 && extra > 0) {
         ones++;
         counter++;
         extra--;
-        console.log("current counter is: " + counter + " , current ones is: " + ones + " , remaining extra: " + extra)
+        //console.log("current counter is: " + counter + " , current ones is: " + ones + " , remaining extra: " + extra)
       } else if ((nums[counter]==0 && extra == 0)){
         break;
       }
     }
     maxOnes = Math.max(ones, maxOnes);
-    console.log("ones is: " + ones + ", max ones: " + maxOnes)
+    //console.log("ones is: " + ones + ", max ones: " + maxOnes)
   }
   return maxOnes;  
 }
 
-console.log(longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3));
+console.log(longestOnes([1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0], 6));
