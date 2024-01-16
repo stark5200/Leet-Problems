bdac66e660c52ff5abe45bbf0a2762dc17c5995f
stark5200 <a.taha5200@gmail.com>
2024-01-16 19:04:56 +0100

    time to optimise

--- leet/problem16.ts
+++ leet/problem16.ts
@@ -1,6 +1,6 @@
 function longestOnes(nums: number[], k: number): number {
   let maxOnes = 0;
-  for (let num = 0; num < nums.length; num++) {
+  for (let num = 0; num <= nums.length - k; num++) {
     let ones = 0;
     let counter = num;
     console.log("initial counter for num " + num + " is: " + counter + " , max ones is: " + maxOnes)
@@ -15,8 +15,8 @@ function longestOnes(nums: number[], k: number): number {
         counter++;
         extra--;
         console.log("current counter is: " + counter + " , current ones is: " + ones + " , remaining extra: " + extra)
-      } else {
-        continue;
+      } else if ((nums[counter]==0 && extra == 0)){
+        break;
       }
     }
     maxOnes = Math.max(ones, maxOnes);
@@ -26,16 +26,3 @@ function longestOnes(nums: number[], k: number): number {
 }
 
 console.log(longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3));
-/*
-function findMaxAverage(nums: number[], k: number): number {
-  let sum = (n: number, ...arr: number[]) => [...arr.slice(n, n+k)].reduce((x, y) => x + y, 0); 
-  let max = sum(0, ...nums);
-  console.log("initial max is: "+max);
-  let currentsum = max;
-  for (let num = 1; num <= nums.length-k; num++) {
-    currentsum = currentsum-nums[num-1]+nums[num+k-1];
-    max = Math.max(currentsum, max);
-  }
-  return max/k;  
-};
-*/
