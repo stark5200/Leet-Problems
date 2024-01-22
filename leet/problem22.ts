93ced167635ff44c127f33ecc72d7bed6b9afad6
stark5200 <a.taha5200@gmail.com>
2024-01-22 21:53:15 +0100

    chatgpt solution

--- leet/problem22.ts
+++ leet/problem22.ts
@@ -28,3 +28,37 @@ function closeStrings(word1: string, word2: string): boolean {
   return true;
   // this problem is a bit difficult
 };
+
+/* chat gpt solution
+function closeStrings(word1: string, word2: string): boolean {
+    if (word1.length !== word2.length) {
+        return false;
+    }
+
+    const count1: number[] = new Array(26).fill(0);
+    const count2: number[] = new Array(26).fill(0);
+
+    for (let i = 0; i < word1.length; i++) {
+        count1[word1.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        count2[word2.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if ((count1[i] === 0 && count2[i] !== 0) || (count1[i] !== 0 && count2[i] === 0)) {
+            return false;
+        }
+    }
+
+    count1.sort((a, b) => a - b);
+    count2.sort((a, b) => a - b);
+
+    for (let i = 0; i < 26; i++) {
+        if (count1[i] !== count2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+*/
