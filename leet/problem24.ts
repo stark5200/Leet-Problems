d85a6bb50dffd33bf2171829824c7d2e171f3bc5
stark5200 <a.taha5200@gmail.com>
2024-01-27 20:44:40 +0100

    works but too slow

--- leet/problem24.ts
+++ leet/problem24.ts
@@ -1,3 +1,28 @@
 function removeStars(s: string): string {
-  return "stars"  
+  let sep = s.split("");
+  let length = sep.length
+  //let indexToRemove: number[] = [];
+  for (let i = length-1; i >= 0; i--) {
+    let check = true;
+    if (sep[i]=="*") {
+      for (let j = i; j >= 0 && check; j-- ) {
+        if (sep[j]!="*") {
+          sep.splice(i, 1);
+          sep.splice(j, 1);  
+          length = sep.length;
+          check = false;
+        }
+      }
+    }
+  }
+  console.log(sep);
+  let result = sep.join("");
+  return result;  
 };
+
+console.log(removeStars("hel*l*o*"));
+
+
+/*
+
+*/ 
