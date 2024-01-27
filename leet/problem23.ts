cc21c0306c3f91aedbf0e56dcc66e0930d53ab64
stark5200 <a.taha5200@gmail.com>
2024-01-27 02:55:47 +0100

    gpt solution

--- leet/problem23.ts
+++ leet/problem23.ts
@@ -3,7 +3,7 @@ function equalPairs(grid: number[][]): number {
   let columns: number[][] = [];
   let rows: number[][] = [];
   for (let i in grid) {
-    let col = grid.map(function(value,index) { return value[i]}); 
+    let col = grid.map(function(value) { return value[i]}); 
 
     let row: number[] = [];
       for (let j = 0; j < grid.length; j++) {
@@ -25,3 +25,42 @@ function equalPairs(grid: number[][]): number {
 };
 
 console.log(equalPairs([[3,2,2,5],[2,7,6,1],[2,7,7,8], [5,8,3,6]]));
+
+/*
+function countEqualRowColumnPairs(grid: number[][]): number {
+    const n = grid.length;
+    let count = 0;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            const row = grid[i];
+            const column = getColumn(grid, j);
+
+            if (arraysEqual(row, column)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+function getColumn(grid: number[][], columnIndex: number): number[] {
+    return grid.map(row => row[columnIndex]);
+}
+
+function arraysEqual(arr1: number[], arr2: number[]): boolean {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}  
+  
+*/
