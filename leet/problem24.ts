34afe09912079b7f1538b5e835eb7986125ce407
stark5200 <a.taha5200@gmail.com>
2024-01-28 03:08:23 +0100

    still not good enough

--- leet/problem24.ts
+++ leet/problem24.ts
@@ -2,11 +2,43 @@ function removeStars(s: string): string {
   if (!s.includes("*")) {
     return s;
   }
-  let sep = s.split("*");
-  //sep.splice(sep.indexOf("*"), 1);
-  console.log("lets see: " + sep);
+  let sArray = s.split("");
+  console.log(sArray);
+  let rArray = sArray.reverse();
+  console.log(rArray);
+  const asteriskCount = countOccurrences(sArray, "*");
+  console.log(asteriskCount);
+  const lastIndex = rArray.indexOf("*")
+  console.log(lastIndex);
+  let fSep = s.split("*");
+  let med = fSep.join("");
+  let sep = med.split("");
+  console.log(sep);
+  //if (sep.includes("")) {
+  //  sep.splice(sep.indexOf(""));
+  //}
+  let rSep = sep.reverse();
+  console.log(rSep);
+  rSep.splice(lastIndex, asteriskCount);
+  console.log(rSep);
+  return rSep.reverse().join("");  
+};
+
+function countOccurrences(arr: string[], target: string): number {
+  return arr.filter(item => item === target).length;
+}
+
+console.log(removeStars("he*l*l*o*"));
+
+
+/* old code 
+
+function removeStars(s: string): string {
+    if (!s.includes("*")) {
+    return s;
+  }
+  let sep = s.split("");
   let length = sep.length
-  
   for (let i = length-1; i >= 0; i--) {
     let check = true;
     if (sep[i]=="*") {
@@ -20,14 +52,8 @@ function removeStars(s: string): string {
       }
     }
   }
-  console.log(sep);
   let result = sep.join("");
-  return result;  
+  return result  
 };
-
-console.log(removeStars("he*l*l*o*"));
-
-
-/*
 
 */ 
