72a9a198410b2af54c2ba968336e6a0495aec932
stark5200 <a.taha5200@gmail.com>
2024-01-31 20:11:41 +0100

    works but bad performance

--- leet/problem25.ts
+++ leet/problem25.ts
@@ -1,41 +1,35 @@
 function asteroidCollision(asteroids: number[]): number[] {
 
   for (let i = 0; i < asteroids.length; i++) {
-    if (asteroids[i]!==0) {
       if (asteroids[i]*asteroids[i+1]<0) {
         if (Math.abs(asteroids[i]) > Math.abs(asteroids[i+1]) && asteroids[i] > 0) {
           asteroids[i+1] = 0;
+          asteroids = asteroids.filter((asteroid) => asteroid !== 0);
+          i > 0 ? i = i-2 : i = -1;
         } else if (Math.abs(asteroids[i]) > Math.abs(asteroids[i+1]) && asteroids[i] < 0) {
           continue;
         }
         
         else if ( Math.abs(asteroids[i]) < Math.abs(asteroids[i+1]) && asteroids[i] > 0) {
           asteroids[i] = 0;
+          asteroids = asteroids.filter((asteroid) => asteroid !== 0);
+          i > 0 ? i = i-2 : i = -1;
         } else if (Math.abs(asteroids[i]) < Math.abs(asteroids[i+1]) && asteroids[i] < 0) {
           continue;
         }
-        
+
         else if (asteroids[i] > 0) {
           asteroids[i] = 0;
           asteroids[i+1] = 0;
+          asteroids = asteroids.filter((asteroid) => asteroid !== 0);
+          i > 1 ? i = i-2 : i = -1;
         } else {
           continue;
         }
       }
-    }
   }
-    
-  asteroids = asteroids.filter((asteroid) => asteroid !== 0);
-  let positive: number[] = []
-  let negative: number[] = []
-  positive.push(...asteroids.filter((asteroid) => asteroid > 0));
-  negative.push(...asteroids.filter((asteroid) => asteroid < 0));
 
-  if (positive.length * negative.length === 0) {
-    return asteroids;
-  }
-  
-  return asteroidCollision(asteroids);
+  return asteroids;
 }
 
 
@@ -43,6 +37,8 @@ console.log(asteroidCollision([5,10,-5]));
 console.log(asteroidCollision([8,-8]));
 console.log(asteroidCollision([10,2,-5]));
 console.log(asteroidCollision([-2,-1,1,2]));
+console.log(asteroidCollision([1,-1,1,-2]));
+console.log(asteroidCollision([2,-1,-2,-2]));
 
 /*
 
